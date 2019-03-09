4daff672ca80495d46f183e2e23d06df7e9c63dd
rafael.tavares <rafaeltavares91@gmail.com>
2019-03-09 03:32:11 -0300

    creating details page of Pessoa and updating PessoaController tests

--- src/main/java/com/example/demo/controller/PessoaController.java
+++ src/main/java/com/example/demo/controller/PessoaController.java
@@ -2,10 +2,18 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.service.PessoaService;
 
+/**
+ * 
+ * @author Rafael Tavares
+ *
+ */
 @Controller
 @RequestMapping("/pessoa")
 public class PessoaController {
@@ -16,10 +24,22 @@ public class PessoaController {
 		this.pessoaService = pessoaService;
 	}
 	
-	@RequestMapping("")
-	public String pessoa(Model model) {
+	@RequestMapping({"", "/", "/list"})
+	public String list(Model model) {
 		model.addAttribute("pessoas", pessoaService.findAll());
 		return "pessoa/list";
+	}
+	
+	@RequestMapping("/find")
+	public String find() {
+		return "notimplemented";
+	}
+	
+	@GetMapping({"/{pessoaId}", "/show/{pessoaId}"})
+	public ModelAndView show(@PathVariable("pessoaId") Long pessoaId) {
+		ModelAndView mav = new ModelAndView("pessoa/show");
+		mav.addObject(pessoaService.findById(pessoaId));
+		return mav;
 	}
 	
 }
--- src/test/java/com/example/demo/controller/PessoaControllerTest.java
+++ src/test/java/com/example/demo/controller/PessoaControllerTest.java
@@ -1,74 +1,82 @@
 package com.example.demo.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.ui.Model;
 
 import com.example.demo.domain.Pessoa;
 import com.example.demo.service.PessoaService;
 import com.google.common.collect.Sets;
 
+/**
+ * 
+ * @author Rafael Tavares
+ *
+ */
+@ExtendWith(MockitoExtension.class)
 public class PessoaControllerTest {
 
-	private PessoaController pessoaController;
+	@InjectMocks
+	private PessoaController controller;
 	
 	@Mock
 	private PessoaService pessoaService;
 	
-	@Mock
-	private Model model;
+	private Set<Pessoa> pessoas;
+
+	private MockMvc mockMvc;
 	
 	@BeforeEach
 	public void setUp() {
-		MockitoAnnotations.initMocks(this);
-		pessoaController = new PessoaController(pessoaService);
-	}
-	
-	@Test
-	public void testMockMVC() throws Exception {
-		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(pessoaController).build();
-		mockMvc.perform(get("/pessoa"))
-			.andExpect(status().isOk())
-			.andExpect(view().name("pessoa/list"));
-	}
-	
-	@Test
-	public void pessoa() {
-		Pessoa p1 = new Pessoa();
-		p1.setId(1l);
-		p1.setNome("Rafael Tavares");
-		Pessoa p2 = new Pessoa();
-		p2.setId(2l);
-		p2.setNome("Vanessa Dantas");
-		Set<Pessoa> pessoas = Sets.newHashSet(p1, p2);
+		pessoas = Sets.newHashSet();
+		pessoas.add(Pessoa.builder().id(1l).build());
+		pessoas.add(Pessoa.builder().id(2l).build());
 		
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+	}
+	
+	@Test
+	public void list() throws Exception {
 		when(pessoaService.findAll()).thenReturn(pessoas);
 		
-		@SuppressWarnings("unchecked")
-		ArgumentCaptor<Set<Pessoa>> argumentoCaptor = ArgumentCaptor.forClass(Set.class);
+		mockMvc.perform(get("/pessoa"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("pessoa/list"))
+			.andExpect(model().attribute("pessoas", hasSize(2)));
+	}
+	
+	@Test
+	public void find() throws Exception {
+		mockMvc.perform(get("/pessoa/find"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("notimplemented"));
 		
-		String viewName = pessoaController.pessoa(model);
-		assertEquals("pessoa/list", viewName);
-		verify(pessoaService, times(1)).findAll();
-		verify(model, times(1)).addAttribute(eq("pessoas"), argumentoCaptor.capture());
-		Set<Pessoa> listInController = argumentoCaptor.getValue();
-		assertEquals(2, listInController.size());
+		verifyZeroInteractions(pessoaService);
+	}
+	
+	@Test
+	public void show() throws Exception {
+		when(pessoaService.findById(anyLong())).thenReturn(Pessoa.builder().id(1l).build());
+		
+		mockMvc.perform(get("/pessoa/show/1231"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("pessoa/show"))
+			.andExpect(model().attribute("pessoa", hasProperty("id", is(1l))));
 	}
 	
 }
